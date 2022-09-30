b54ca451168ce6fac8c1f306025787e637279821
Shuhei Shibahara <Shoehey06@gmail.com>
2022-09-29 17:37:37 -0700

    tried to move bird

--- flappyBird/practice-for-ch-js-flappy-bird-long-practice/src/game.js
+++ flappyBird/practice-for-ch-js-flappy-bird-long-practice/src/game.js
@@ -8,12 +8,59 @@ export default class FlappyBird {
 
   animate() {
     this.level.drawBackground(this.ctx)
+    this.bird.animate(this.ctx);
+    if (this.running === true){
+      window.requestAnimationFrame(this.animate);
+    }
   }
 
   restart(){
+    this.running = false;
     this.level = new Level(this.dimensions)
+    this.bird = new Bird(this.dimensions)
     this.animate()
+  }
+
+  play(){
+    this.running = true;
+    this.animate();
+  }
+}
+class Bird {
+  constructor(dimensions){
+    this.velocity = 0;
+    this.dimensions = dimensions
+    this.positiony = this.dimensions.height/2 
+    this.positionx = this.dimensions.width/3
+  }
+  
+
+  animate(ctx){
+    this.move();
+    this.drawBird(ctx);
+  }
+
+  move(){
+    this.positiony += this.velocity;
+    this.velocity += CONSTANTS.GRAVITY;
+  }
+
+  flap(){
+    this.velocity = -8;
+
+  }
+
+  drawBird(ctx){
+    ctx.fillStyle = 'yellow'
+    ctx.fillRect(this.positionx, this.positiony, 50, 60);
   }
 }
 
 
+const CONSTANTS = {
+  GRAVITY: 0.8,
+  FLAP_SPEED: -8,
+  TERMINAL_VEL: 12,
+  BIRD_WIDTH: 40,
+  BIRD_HEIGHT: 30
+};
--- flappyBird/practice-for-ch-js-flappy-bird-long-practice/src/index.js
+++ flappyBird/practice-for-ch-js-flappy-bird-long-practice/src/index.js
@@ -3,5 +3,7 @@ import FlappyBird from './game';
 const canvas = document.getElementById('bird-game');
 
 const flappy = new FlappyBird(canvas);
+
 flappy.restart()
 
+flappy.play();
